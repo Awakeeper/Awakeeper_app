63ceeb377aebadade10dc57166b585edf7649d0e
ChaChon <swc8912@gmail.com>
2016-08-22 21:40:57 +0900

    인터넷 가능, 메인쪽 시나리오에 데이터 추가해서 처리해야함

--- app/src/main/java/com/tenpm/awakeeper/MainActivity.java
+++ app/src/main/java/com/tenpm/awakeeper/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView heartbeatText;
     private TextView irisText;
     private TextView stateText;
+    private TextView timeText;
 
     public static SoundPool soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
     private int soundId;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         setLayout();
         setSounds();
@@ -60,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         heartbeatText = (TextView)findViewById(R.id.heartbeatText);
         irisText = (TextView)findViewById(R.id.irisText);
         stateText = (TextView)findViewById(R.id.stateText);
+        timeText = (TextView)findViewById(R.id.timeText);
 
         prefButton = (Button)findViewById(R.id.prefButton);
         prefButton.setOnClickListener(new View.OnClickListener() {
@@ -100,32 +103,43 @@ public class MainActivity extends AppCompatActivity {
     public int totalTime = 0;
 
     private void playScenario(){
+        currentTime = 0;
         // 시간에 따라서 데이터에 맞는 행동을 수행한다.
         totalTime = carDataArrayList.size();
 
         TimerTask myTask = new TimerTask() {
             public void run() {
+                // 데이터값 ui에 표시
+                gpsText.setText("GPS: " + carDataArrayList.get(currentTime).getGpsX() + "," + carDataArrayList.get(currentTime).getGpsY());
+                velocityText.setText("속도: " + carDataArrayList.get(currentTime).getVelocity());
+                angleText.setText("핸들 각도: " + carDataArrayList.get(currentTime).getAngle());
+                heartbeatText.setText("심박수 " + sensorDataArrayList.get(currentTime).getHartBeat());
+                irisText.setText("홍채값: " + sensorDataArrayList.get(currentTime).getIris());
+                timeText.setText("시간: " + currentTime + "초");
+
                 if(CurrentState == STATE_IDLE && carDataArrayList.get(currentTime).getVelocity() > 0){
                     driveState();
                 }
                 else if(CurrentState != STATE_IDLE && carDataArrayList.get(currentTime).getVelocity() <= 0){
                     idleState();
                 }
-                else if(CurrentState == STATE_DRIVE && drowsyLevel < 2 && detectDrowsy(currentTime)){
+                else if(CurrentState == STATE_DRIVE && detectDrowsy(currentTime)){
                     // 졸음운전 확인
-                    if(drowsyLevel == 0){
+                    if(drowsyLevel == 0) {
                         drowsyLevel1();
-                        drowsyLevel++;
-                    }
-                    else if(drowsyLevel == 1){
-                        drowsyLevel2();
+                        drowsyLevel = 1;
                     }
                 }
+                else if(CurrentState == STATE_DRIVE && drowsyLevel == 1){
+                    drowsyLevel = 2;
+                    drowsyLevel2();
+                }
                 else if((CurrentState == STATE_DROWSY1 || CurrentState == STATE_DROWSY2) && !detectDrowsy(currentTime)){
+                    // 졸음 깬 경우
                     driveState();
                 }
 
-                if(currentTime >= totalTime){
+                if(currentTime >= totalTime){ // 시나리오 끝
                     idleState();
                     this.cancel();
                 }
@@ -135,16 +149,14 @@ public class MainActivity extends AppCompatActivity {
         };
         Timer timer = new Timer();
         timer.schedule(myTask, 0, 1000); // 5초후 첫실행, 1초마다 계속실행
-
-        for(int i=0; i<carDataArrayList.size(); i++){
-
-        }
     }
 
     private void idleState(){
+        CurrentState = STATE_IDLE;
         stateText.setText(getString(R.string.idle_text));
         prefButton.setVisibility(View.VISIBLE);
         soundPool.stop(soundId);
+        soundPool.stop(songId);
         drowsyLevel = 0;
 
     }
@@ -164,6 +176,15 @@ public class MainActivity extends AppCompatActivity {
     private void drowsyLevel1(){
         soundPool.play(soundId, 1, 1, 0, 0, 1);
         CurrentState = STATE_DROWSY1;
+
+        TimerTask myTask = new TimerTask() {
+            public void run() {
+                soundPool.stop(soundId);
+                CurrentState = STATE_DRIVE;
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(myTask, 10000);
     }
 
     private void drowsyLevel2(){
@@ -172,7 +193,10 @@ public class MainActivity extends AppCompatActivity {
         TimerTask myTask = new TimerTask() {
             public void run() {
                 soundPool.stop(soundId);
-                playSinging();
+                if(isSongSet) // 음악 설정시 노래 부르기 시작
+                    playSinging();
+                else // 없으면 정지할 때 까지 사이렌 계속됨
+                    sirenUnlessStop();
             }
         };
         Timer timer = new Timer();
@@ -183,5 +207,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), SingingActivity.class);
         intent.putExtra("songId", songId);
         startActivity(intent);
+    }
+
+    private void sirenUnlessStop(){
+
     }
 }
--- app/src/main/java/com/tenpm/awakeeper/SplashActivity.java
+++ app/src/main/java/com/tenpm/awakeeper/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.tenpm.awakeeper;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -19,17 +24,21 @@ import cz.msebera.android.httpclient.Header;
 public class SplashActivity extends AppCompatActivity {
     private Handler mHandler;
     private Runnable mRunnable;
+    private final int PERMISSION_INTERNET = 99;
+    private final String TAG = "SplashActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onresume");
+        //checkPermission();
         getJSON();
 
         mRunnable = new Runnable() {
@@ -42,7 +51,7 @@ public class SplashActivity extends AppCompatActivity {
         };
 
         mHandler = new Handler();
-        mHandler.postDelayed(mRunnable, 5000);
+        //mHandler.postDelayed(mRunnable, 5000);
     }
 
     private static AsyncHttpClient client = new AsyncHttpClient();
@@ -84,4 +93,55 @@ public class SplashActivity extends AppCompatActivity {
         mHandler.removeCallbacks(mRunnable);
         super.onDestroy();
     }
+
+//    @TargetApi(Build.VERSION_CODES.M)
+//    private void checkPermission() {
+//        Log.i(TAG, "CheckPermission : " + checkSelfPermission(Manifest.permission.INTERNET));
+//        if (checkSelfPermission(Manifest.permission.INTERNET)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            Log.d(TAG, "checkPermission if");
+//            // Should we show an explanation?
+//            if (shouldShowRequestPermissionRationale(Manifest.permission.INTERNET)) {
+//                // Explain to the user why we need to write the permission.
+//                Toast.makeText(this, "인터넷 연결", Toast.LENGTH_SHORT).show();
+//            }
+//            Log.d(TAG, "requestPermissions");
+//            requestPermissions(new String[]{Manifest.permission.INTERNET},
+//                    PERMISSION_INTERNET);
+//            Log.d(TAG, "requestPermissions end");
+//            // MY_PERMISSION_REQUEST_STORAGE is an
+//            // app-defined int constant
+//
+//        } else {
+//            // 다음 부분은 항상 허용일 경우에 해당이 됩니다.
+//            Log.d(TAG, "getJson  granted");
+//            //getJSON();
+//        }
+//    }
+
+
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        switch (requestCode) {
+//            case PERMISSION_INTERNET:
+//                Log.d(TAG, "" + permissions.length);
+//                Log.d(TAG, "" + grantResults.length);
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                    //getJSON();
+//
+//                    // permission was granted, yay! do the
+//                    // calendar task you need to do.
+//
+//                } else {
+//
+//                    Log.d(TAG, "Permission always deny");
+//
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+//                break;
+//        }
+//    }
 }
